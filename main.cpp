a0f59fb8cee256a6436e1e4223118af4d5a833ed
DudchenkoViktor <viktordudcenko525@gmail.com>
2025-04-13 22:55:51 +0000

    Typo fixed: replaced end1 with endl in main.cpp

--- main.cpp
+++ main.cpp
@@ -6,6 +6,6 @@ int main() {
 	MyClass obj;
 	double x = 0.5;
 	int n = 5;
-	std::cout << "Sum of first " << n << " terms: " << obj.FuncA(x,n) << std::end1;
+	std::cout << "Sum of first " << n << " terms: " << obj.FuncA(x,n) << std::endl;
 	return 0;
 }
